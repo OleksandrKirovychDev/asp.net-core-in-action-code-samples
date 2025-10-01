c0e7cb80aa18767a2c0adc3a48ca50ba0f72ba2c
OleksandrKirovychDev <oleksandr.kirovych.dev@gmail.com>
2025-10-01 22:17:29 +0200

    feat(part-1): chapter 3 code

--- Chapter-1/MinimalApi1/Program.cs
+++ Chapter-1/MinimalApi1/Program.cs
@@ -1,6 +1,23 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.AspNetCore.HttpLogging;
 
-app.MapGet("/", () => "Hello World!");
+public partial class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddHttpLogging(opts => opts.LoggingFields = HttpLoggingFields.RequestProperties);
+        builder.Logging.AddFilter("Microsoft.AspNetCore.HttpLogging", LogLevel.Information);
+        var app = builder.Build();
 
-app.Run();
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseHttpLogging();
+        }
+
+        app.MapGet("/", () => "Hello World!");
+        app.MapGet("/person", () => new Person("Alex", "Alexovski"));
+        app.Run();
+    }
+}
+
+public record Person(String FirstName, String LastName);
